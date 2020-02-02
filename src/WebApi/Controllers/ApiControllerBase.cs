8532c77f13aff2f4a283c6fc62a5418ad72d192f
Maxime Labelle <springcomp@users.noreply.github.com>
2020-02-02 14:34:28 +0100

    Basic API (stub) to retrieve messages and message body.

---
+++ src/WebApi/Controllers/ApiControllerBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace WebApi.Controllers
+{
+    public class ApiControllerBase : ControllerBase
+    {
+        protected ApiControllerBase()
+        { }
+
+        protected bool GetAuthenticatedUserId(out string identifier)
+        {
+            const string ns = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+            var sub = User.Claims.FirstOrDefault(c => c.Type == ns)?.Value;
+            identifier = sub;
+
+            return true;
+        }
+    }
+}
---
+++ src/WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Services.Interop;
+
+namespace WebApi.Controllers
+{
+    [Route("messages")]
+    [ApiController]
+    [Authorize]
+    public class MessagesController : ApiControllerBase
+    {
+        private readonly IProfilesService service_;
+        private readonly ILogger<MessagesController> logger_;
+
+        public MessagesController(IProfilesService service, ILogger<MessagesController> logger)
+        {
+            service_ = service;
+            logger_ = logger;
+        }
+
+        // GET messages/my
+        [HttpGet()]
+        [Route("my")]
+        public async Task<IActionResult> GetMessages()
+        {
+            if (!GetAuthenticatedUserId(out var identifier))
+                return BadRequest();
+
+            var addresses = (await service_
+                    .GetMaskedEmails(identifier))
+                    .Select(a => a.EmailAddress)
+                    .ToArray()
+                    ;
+
+            return Ok(addresses);
+        }
+
+        // GET messages/my/{location}
+        [HttpGet()]
+        [Route("my/{location}")]
+        public async Task<IActionResult> GetMessageBody(string location)
+        {
+            if (!GetAuthenticatedUserId(out var identifier))
+                return BadRequest();
+
+            return Ok();
+        }
+    }
+}
