13e9974d08775b17ab3ac00868f939b78e44b139
springcomp <springcomp@users.noreply.github.com>
2021-12-07 22:09:49 +0100

    Added missing using statement for SPA Angular Server.

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -7,6 +7,7 @@ using InboxApi.Model.Interop;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
